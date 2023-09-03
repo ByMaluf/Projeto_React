256a0b4eac14f17be255f34a7e864c84acec714e
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2023-09-02 22:40:14 -0400

    Renderização Condicional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
-import Membro from "./Components/Membro";  
+import './index.css';
 
-class App extends Component {
+class Membro extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true
+    };
 
-  
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+
+  }
+
+  sair() {
+    this.setState({ status: false })
+  }
+
+  entrar() {
+    this.setState({ status: true })
+  }
+
   render() {
     return (
       <div>
-        <Membro nome="Visitante"/>
+        {/* {this.state.status === 1 && <h1>Bem-Vindo ao sistema!</h1>} */}
+
+        {/* Ou pode-se fazer com o OPERADOR TERNÁRIO */}
+
+        {this.state.status ?
+          <div>
+            <h2>Bem-vindo ao Sistema</h2>
+            <button onClick={this.sair} > Sair do sistema</button>
+          </div>
+          :
+          <div>
+            <h2>Olá Visitante, faça o login!</h2>
+            <button onClick={this.entrar}>Entra no sistema</button>
+          </div>
+        }
       </div>
+
     );
   }
 }
 
-export default App;
+export default Membro;
+
+
